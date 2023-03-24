dddf2457c53d674d49824ee892781913e6a8c6f4
smcbrmmm <samutch19@gmail.com>
2023-03-25 00:29:12 +0700

    add secure ssl

--- greet/server/main.go
+++ greet/server/main.go
@@ -6,6 +6,7 @@ import (
 
 	pb "github.com/smcbrmmm/grpc-go-course/greet/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 var addr string = "0.0.0.0:50051"
@@ -22,7 +23,21 @@ func main() {
 
 	log.Printf("listening on %s\n", addr)
 
-	s := grpc.NewServer()
+	opts := []grpc.ServerOption{}
+
+	tls := true // change that to true if needed
+	if tls {
+		certFile := "ssl/server.crt"
+		keyFile := "ssl/server.pem"
+		creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+
+		if err != nil {
+			log.Fatalf("Failed loading certificates: %v\n", err)
+		}
+		opts = append(opts, grpc.Creds(creds))
+	}
+
+	s := grpc.NewServer(opts...)
 	pb.RegisterGreetServiceServer(s, &Server{})
 
 	if err = s.Serve(lis); err != nil {
